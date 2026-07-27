mtaas-v0/qdrant-woa-ci

perf(hnsw): prefetch neighbor vectors during graph search
Issue a software prefetch for each unvisited neighbor's vector data in
search_on_level before the batch scoring pass, so the scattered-load
latency overlaps the rest of the neighbor-collection loop.

The prefetch is plumbed through default-no-op hooks (FilteredScorer::
prefetch_points -> RawScorer -> QueryScorer::prefetch_stored ->
DenseVectorStorageRead::prefetch_dense), so only dense metric scoring
over in-RAM/mmap storage is affected; multivector, sparse and quantized
paths are untouched.

It is gated by DenseVectorStorageRead::get_dense_is_borrowed(): io_uring
reads copy into an owned buffer, so prefetching there would trigger a
redundant read. The method returns false for io_uring, keeping prefetch
a no-op on that path.

Benchmark (hnsw_search_graph, 1M x 64d cosine, k=10, ef=100, single
thread, 10 runs): +20.6% on in-RAM storage and +17.4% on
page-cache-warm mmap storage; every run was faster with prefetch
(paired-t ~10.3 and ~9.0, p<0.001). Also adds a permanent mmap variant
to the benchmark.

Co-Authored-By: Claude Opus 4.8 (1M context) <noreply@anthropic.com>

lib/segment/benches/fixture.rs
@@ -7,13 +7,17 @@ use fs_err as fs;
 use rand::SeedableRng as _;
 use rand::rngs::StdRng;
 use rayon::iter::{IntoParallelIterator as _, ParallelIterator as _};
-use segment::fixtures::index_fixtures::TestRawScorerProducer;
+use segment::data_types::vectors::VectorElementType;
+use segment::fixtures::index_fixtures::{TestRawScorerProducer, random_vector};
 use segment::index::hnsw_index::HnswM;
 use segment::index::hnsw_index::graph_layers::GraphLayers;
 use segment::index::hnsw_index::graph_layers_builder::GraphLayersBuilder;
 use segment::index::hnsw_index::graph_links::GraphLinksFormatParam;
 use segment::index::hnsw_index::hnsw::SINGLE_THREADED_HNSW_BUILD_THRESHOLD;
 use segment::spaces::metric::Metric;
+use segment::vector_storage::dense::dense_vector_storage::open_dense_vector_storage;
+use segment::vector_storage::{DEFAULT_STOPPED, DenseVectorStorage, VectorStorageEnum};
+use tempfile::TempDir;
 
 /// Generate vectors and HNSW graph to be used in benchmarks.
 ///
@@ -86,6 +90,50 @@ where
     };
 
     (vector_holder, graph_layers)
+}
+
+/// Build an mmap-backed dense vector storage holding the same seed-42 vectors
+/// as [`make_cached_graph`], so the cached graph is valid against it.
+///
+/// Returns the [`TempDir`] (which must be kept alive for the mmap to stay open)
+/// and a [`TestRawScorerProducer`] whose `scorer` reads from the mmap storage.
+// Shared bench module: not every bench that includes `fixture` uses this.
+#[allow(dead_code)]
+pub fn make_memmap_producer<METRIC>(
+    num_vectors: usize,
+    dim: usize,
+) -> (TempDir, TestRawScorerProducer)
+where
+    METRIC: Metric<f32>,
+{
+    let distance = METRIC::distance();
+
+    // Force the plain mmap variant (not io_uring), so prefetch is exercised.
+    #[cfg(target_os = "linux")]
+    segment::vector_storage::common::set_async_scorer(false);
+
+    let tmp = tempfile::Builder::new()
+        .prefix("hnsw-mmap-bench")
+        .tempdir()
+        .unwrap();
+    let mut storage = open_dense_vector_storage(tmp.path(), dim, distance, false).unwrap();
+
+    // Regenerate the exact vectors from `make_cached_graph` (same seed + preprocess).
+    let mut rng = StdRng::seed_from_u64(42);
+    let mut vectors = (0..num_vectors).map(|_| {
+        let v = random_vector(&mut rng, dim);
+        let v = distance.preprocess_vector::<VectorElementType>(v);
+        (std::borrow::Cow::Owned(v), false)
+    });
+    let VectorStorageEnum::DenseMemmap(mmap_storage) = &mut storage else {
+        panic!("expected DenseMemmap storage");
+    };
+    mmap_storage
+        .update_from(&mut vectors, &DEFAULT_STOPPED)
+        .unwrap();
+
+    let producer = TestRawScorerProducer::from_storage(storage, num_vectors);
+    (tmp, producer)
 }
 
 fn updated_ago(path: &Path) -> Result<String, Box<dyn std::error::Error>> {

lib/segment/benches/hnsw_search_graph.rs
@@ -30,27 +30,32 @@ fn hnsw_benchmark(c: &mut Criterion) {
     let (vector_holder, mut graph_layers) =
         fixture::make_cached_graph::<Metric>(NUM_VECTORS, DIM, M, EF_CONSTRUCT, USE_HEURISTIC);
 
-    let mut rng = StdRng::seed_from_u64(42);
-    group.bench_function("uncompressed", |b| {
-        b.iter(|| {
-            let query = random_vector(&mut rng, DIM);
+    let (_mmap_tmp, mmap_holder) = fixture::make_memmap_producer::<Metric>(NUM_VECTORS, DIM);
 
-            let scorer = vector_holder.scorer(query);
+    // Search the same graph over in-RAM and mmap-backed vector storage.
+    for (name, holder) in [("uncompressed", &vector_holder), ("mmap", &mmap_holder)] {
+        let mut rng = StdRng::seed_from_u64(42);
+        group.bench_function(name, |b| {
+            b.iter(|| {
+                let query = random_vector(&mut rng, DIM);
 
-            black_box(
-                graph_layers
-                    .search(
-                        TOP,
-                        EF,
-                        SearchAlgorithm::Hnsw,
-                        scorer,
-                        None,
-                        &DEFAULT_STOPPED,
-                    )
-                    .unwrap(),
-            );
-        })
-    });
+                let scorer = holder.scorer(query);
+
+                black_box(
+                    graph_layers
+                        .search(
+                            TOP,
+                            EF,
+                            SearchAlgorithm::Hnsw,
+                            scorer,
+                            None,
+                            &DEFAULT_STOPPED,
+                        )
+                        .unwrap(),
+                );
+            })
+        });
+    }
 
     graph_layers.compress_ram();
     let mut rng = StdRng::seed_from_u64(42);

glatinone/qdrant

Co-authored-by: Cursor <cursoragent@cursor.com>

lib/segment/benches/fixture.rs
@@ -6,13 +6,17 @@ use fs_err as fs;
 use rand::SeedableRng as _;
 use rand::rngs::SmallRng;
 use rayon::iter::{IntoParallelIterator as _, ParallelIterator as _};
-use segment::fixtures::index_fixtures::TestRawScorerProducer;
+use segment::data_types::vectors::VectorElementType;
+use segment::fixtures::index_fixtures::{TestRawScorerProducer, random_vector};
 use segment::index::hnsw_index::HnswM;
 use segment::index::hnsw_index::graph_layers::GraphLayers;
 use segment::index::hnsw_index::graph_layers_builder::GraphLayersBuilder;
 use segment::index::hnsw_index::graph_links::{GraphLinksFormatParam, GraphLinksResidency};
 use segment::index::hnsw_index::hnsw::SINGLE_THREADED_HNSW_BUILD_THRESHOLD;
 use segment::spaces::metric::Metric;
+use segment::vector_storage::dense::dense_vector_storage::open_dense_vector_storage;
+use segment::vector_storage::{DEFAULT_STOPPED, DenseVectorStorage, VectorStorageEnum};
+use tempfile::TempDir;
 
 /// Generate vectors and HNSW graph to be used in benchmarks.
 ///
@@ -87,6 +91,50 @@ where
     };
 
     (vector_holder, graph_layers)
+}
+
+/// Build an mmap-backed dense vector storage holding the same seed-42 vectors
+/// as [`make_cached_graph`], so the cached graph is valid against it.
+///
+/// Returns the [`TempDir`] (which must be kept alive for the mmap to stay open)
+/// and a [`TestRawScorerProducer`] whose `scorer` reads from the mmap storage.
+// Shared bench module: not every bench that includes `fixture` uses this.
+#[allow(dead_code)]
+pub fn make_memmap_producer<METRIC>(
+    num_vectors: usize,
+    dim: usize,
+) -> (TempDir, TestRawScorerProducer)
+where
+    METRIC: Metric<f32>,
+{
+    let distance = METRIC::distance();
+
+    // Force the plain mmap variant (not io_uring), so prefetch is exercised.
+    #[cfg(target_os = "linux")]
+    segment::vector_storage::common::set_async_scorer(false);
+
+    let tmp = tempfile::Builder::new()
+        .prefix("hnsw-mmap-bench")
+        .tempdir()
+        .unwrap();
+    let mut storage = open_dense_vector_storage(tmp.path(), dim, distance, false).unwrap();
+
+    // Regenerate the exact vectors from `make_cached_graph` (same seed + preprocess).
+    let mut rng = SmallRng::seed_from_u64(42);
+    let mut vectors = (0..num_vectors).map(|_| {
+        let v = random_vector(&mut rng, dim);
+        let v = distance.preprocess_vector::<VectorElementType>(v);
+        (std::borrow::Cow::Owned(v), false)
+    });
+    let VectorStorageEnum::DenseMemmap(mmap_storage) = &mut storage else {
+        panic!("expected DenseMemmap storage");
+    };
+    mmap_storage
+        .update_from(&mut vectors, &DEFAULT_STOPPED)
+        .unwrap();
+
+    let producer = TestRawScorerProducer::from_storage(storage, num_vectors);
+    (tmp, producer)
 }
 
 fn updated_ago(path: &Path) -> Result<String, Box<dyn std::error::Error>> {

lib/segment/benches/hnsw_search_graph.rs
@@ -30,27 +30,32 @@ fn hnsw_benchmark(c: &mut Criterion) {
     let (vector_holder, mut graph_layers) =
         fixture::make_cached_graph::<Metric>(NUM_VECTORS, DIM, M, EF_CONSTRUCT, USE_HEURISTIC);
 
-    let mut rng = SmallRng::seed_from_u64(42);
-    group.bench_function("uncompressed", |b| {
-        b.iter(|| {
-            let query = random_vector(&mut rng, DIM);
+    let (_mmap_tmp, mmap_holder) = fixture::make_memmap_producer::<Metric>(NUM_VECTORS, DIM);
 
-            let scorer = vector_holder.scorer(query);
+    // Search the same graph over in-RAM and mmap-backed vector storage.
+    for (name, holder) in [("uncompressed", &vector_holder), ("mmap", &mmap_holder)] {
+        let mut rng = SmallRng::seed_from_u64(42);
+        group.bench_function(name, |b| {
+            b.iter(|| {
+                let query = random_vector(&mut rng, DIM);
 
-            black_box(
-                graph_layers
-                    .search(
-                        TOP,
-                        EF,
-                        SearchAlgorithm::Hnsw,
-                        scorer,
-                        None,
-                        &DEFAULT_STOPPED,
-                    )
-                    .unwrap(),
-            );
-        })
-    });
+                let scorer = holder.scorer(query);
+
+                black_box(
+                    graph_layers
+                        .search(
+                            TOP,
+                            EF,
+                            SearchAlgorithm::Hnsw,
+                            scorer,
+                            None,
+                            &DEFAULT_STOPPED,
+                        )
+                        .unwrap(),
+                );
+            })
+        });
+    }
 
     graph_layers.compress_ram();
     let mut rng = SmallRng::seed_from_u64(42);